1ee3832b9414d446afd9480ff8ff8da65340c462
Sopan Bhere <115341645+Sam21sop@users.noreply.github.com>
2023-07-03 06:20:53 +0000

    MembershipOperator added

---
+++ 01_Introduction_To_Python/01_GettingStartedWithPython/MembershipOperator.py
@@ -0,0 +1,11 @@
+'''
+Python Logical Operator:
+      Description                   ---->     operator
+1.  Return true if the specified    ---->       in
+    value present in the object.
+
+2.  Return True if the specified    ---->       not in
+    value not present in the object.
+'''
+
+
