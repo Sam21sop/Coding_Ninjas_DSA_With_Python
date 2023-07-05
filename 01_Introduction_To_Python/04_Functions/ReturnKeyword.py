534799793356b6a22120087b93082feba9b3c851
Sopan Bhere <115341645+Sam21sop@users.noreply.github.com>
2023-07-05 09:41:58 +0000

    function example add

---
+++ 01_Introduction_To_Python/04_Functions/ReturnKeyword.py
@@ -0,0 +1,14 @@
+#functino defination
+
+def function1():
+    return 1
+
+def function2():
+    return 'Python'
+
+#functino calling
+result1 = function1()
+print(result1)
+
+result2 = function2()
+print(result2)
