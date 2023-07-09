138ef417af7c9b463d8abbb48ea02137725d1ebc
Sopan Bhere <115341645+Sam21sop@users.noreply.github.com>
2023-07-09 06:35:18 +0000

    Nth term fibonacci number

--- 01_Introduction_To_Python/07_Operators&ForLoop/05NthFibonacciNumber.py
+++ 01_Introduction_To_Python/07_Operators&ForLoop/05NthFibonacciNumber.py
@@ -2,3 +2,15 @@
 F(n) = F(n - 1) + F(n - 2), 
 Where, F(1) = 1, F(2) = 1
 '''
+
+def fibonacci(n):
+    fib_lst = [0, 1]
+    for num in range(2, n+1):
+        fib_lst.append(fib_lst[num-2] + fib_lst[num-1])
+    return fib_lst[n]
+
+if __name__ == '__main__':
+    n = int(input())
+    result = fibonacci(n)
+    print(result)
+
