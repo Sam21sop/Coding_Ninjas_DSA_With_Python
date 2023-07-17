fd8b01957b6b3f1692f64906879ae8dd43ba1c85
Sopan Bhere <sbussiness21@gmail.com>
2023-07-17 19:06:14 +0530

    find the intersection between two array

---
+++ 01_Introduction_To_Python/09_Array/IntersectionOfTwoArray.py
@@ -0,0 +1,29 @@
+import sys
+
+def intersections(arr1, n, arr2, m):
+    for i in range(n):
+        for j in range(m):
+            if arr1[i] == arr2[j]:
+                print(arr1[i], end=' ')
+                arr2[j] = sys.maxsize
+                break
+            
+
+def takeinput():
+    n = int(sys.stdin.readline().strip())
+    if n == 0:
+        return list(), 0
+    arr = list(map(int, sys.stdin.readline().strip().split(" ")))
+    return arr, n 
+
+if __name__ == '__main__':
+    t = int(sys.stdin.readline().strip())
+    while t > 0:
+        arr1, n = takeinput()
+        arr2, m = takeinput()
+        intersections(arr1, n, arr2, m)
+        print()
+        t -= 1
+        
+
+
