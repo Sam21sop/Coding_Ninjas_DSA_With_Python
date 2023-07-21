6963fd36456acdd2010f0504ca0bee06e4ed586a
Sopan Bhere <sbussiness21@gmail.com>
2023-07-21 15:51:40 +0530

    find the length of the 2d array.

--- 01_Introduction_To_Python/11_2DArray/01FindLength.py
+++ 01_Introduction_To_Python/11_2DArray/01FindLength.py
@@ -1,10 +1,12 @@
-array = [[1, 2, 3], 
+import numpy as np
+
+
+
+array = [[1, 2, 3, 6], 
          [4, 5, 6, 7], 
-         [8]]
+         [8, 9 ,8 ,4]]
 
 
+s = np.shape(array)
+print(f'Number of rows: {s[0]} and number of columns: {s[1]}')
 print(len(array))
-# for i in array:
-#     for j in i:
-#         print(j, end=' ')
-#     print()
