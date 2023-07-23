0ddc2e6fba6fb849219e1d1e4af4d87f07a8a455
Sopan Bhere <sbussiness21@gmail.com>
2023-07-23 14:46:39 +0530

    introduction of string in python

---
+++ 01_Introduction_To_Python/12_String/00IntroductionToStrings.py
@@ -0,0 +1,12 @@
+string1 = 'this is a string'
+
+string2  = "this is a string"
+
+string3 ='''this is multiline string.
+            this is string
+            second line.'''
+
+
+print(string1, type(string1))
+print(string2, type(string2))
+print(string3, type(string3))
