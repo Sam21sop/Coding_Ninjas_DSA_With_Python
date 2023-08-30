dfdbe6bb01ec4bcf6de804211cf3107bca708537
Sopan Bhere <sbussiness21@gmail.com>
2023-08-30 17:19:03 +0530

    reverse queue inplace

--- Milestone02_DSA_in_Python/23_Queue/01_ReverseQueue.py
+++ Milestone02_DSA_in_Python/23_Queue/01_ReverseQueue.py
@@ -1,6 +1,23 @@
 from QueueUsingArray import Queue
 
-q = Queue()
-
 def fun(q):
-    if not
+    if not q.is_empty():
+        i = q.dequeue()
+        fun(q)
+        q.enqueue(i)
+
+
+def main():
+    q = Queue()
+
+    q.enqueue(1)
+    q.enqueue(2)
+    q.enqueue(3)
+    q.enqueue(4)
+    q.enqueue(5)
+    print("Original Queue:", *q.data)
+    fun(q)
+    print("Queue after applying f:", *q.data)
+
+if __name__ == '__main__':
+    main()
