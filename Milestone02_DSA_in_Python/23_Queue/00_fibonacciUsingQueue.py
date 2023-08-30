4d7c7ecb65dfff6843f1cb56e4ce8804a8a8f330
Sopan Bhere <sbussiness21@gmail.com>
2023-08-30 17:12:12 +0530

    fibonacci number using queue data structure

--- Milestone02_DSA_in_Python/23_Queue/00_fibonacciUsingQueue.py
+++ Milestone02_DSA_in_Python/23_Queue/00_fibonacciUsingQueue.py
@@ -5,11 +5,11 @@ def fun(n):
     q = Queue()
     q.enqueue(0)
     q.enqueue(1)
-    for i in range(n):
+    for i in range(n+1):
         a = q.dequeue()
         b = q.dequeue()
         q.enqueue(b)
         q.enqueue(a+b)
         print(a)
 
-fun(5)
+fun(10)
