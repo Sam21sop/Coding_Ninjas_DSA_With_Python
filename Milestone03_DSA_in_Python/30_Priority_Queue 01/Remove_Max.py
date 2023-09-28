f3898b893550e526e45bccff8a8d664478d435de
Sam21sop <sbussiness21@gmail.com>
2023-09-28 09:35:09 +0530

    remove max class implement

--- Milestone03_DSA_in_Python/30_Priority_Queue 01/Remove_Max.py
+++ Milestone03_DSA_in_Python/30_Priority_Queue 01/Remove_Max.py
@@ -4,43 +4,66 @@ class PriorityQueue:
 
 
     def isEmpty(self):
-        pass
+        return len(self.heap) ==  0
 
 
     def getSize(self):
-        pass
+        return len(self.heap)
 
 
     def getMax(self):
-        pass
+        if self.isEmpty():
+            return
+        return self.heap[0][0]
 
 
     def parent(self, index):
-        pass
+        return  (index - 1) // 2
 
 
     def __heapify_up(self, index):
-        pass
+        while index > 0 and self.heap[index][1] > self.heap[self.parent(index)][1]:
+            self.heap[index], self.heap[self.parent(index)] = self.heap[self.parent(index)], self.heap[index]
+            index = self.parent(index)
 
 
-    def insert(self):
-        pass
+    def insert(self, element, priority):
+        node = (element, priority)
+        self.heap.append(node)
+        self.__heapify_up(len(self.heap)-1)
 
 
-    def left_child(self):
-        pass
+    def left_child(self, index):
+        return 2 * index + 1
 
 
-    def right_child(self):
-        pass
+    def right_child(self, index):
+        return 2 * index + 2
 
 
     def __heapify_down(self, index):
-        pass
+        max_index = index
+        left = self.left_child(index)
+        right = self.right_child(index)
+        if left < len(self.heap) and self.heap[left][1] > self.heap[max_index][1]:
+            max_index = left
+        if right < len(self.heap) and self.heap[right][1] > self.heap[max_index][1]:
+            max_index = right
+        if max_index != index:
+            self.heap[index], self.heap[max_index] = self.heap[max_index], self.heap[index]
+            self.__heapify_down(max_index)
 
-    
+        
     def remove_max(self):
-        pass
+        if len(self.heap) == 0:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()[0]
+
+        max_value = self.heap[0][0]
+        self.heap[0] = self.heap.pop()
+        self.__heapify_down(0)
+        return max_value
 
 
 def main():
@@ -56,7 +79,7 @@ def main():
         elif choice == 2:
             print(pq.getMax())
         elif choice == 3:
-            print(pq.removeMax())
+            print(pq.remove_max())
         elif choice == 4:
             print(pq.getSize())
         elif choice == 5:
