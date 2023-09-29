7e82bdedf3cea1d9521ca14f5d323b8a58102f13
Sam21sop <sbussiness21@gmail.com>
2023-09-29 11:10:22 +0530

    adding, searching, start_with, and remove word etc operation class implement done.

--- Milestone03_DSA_in_Python/32_Tries_and_HuffmanCoding/00_Add_word_in_Tries.py
+++ Milestone03_DSA_in_Python/32_Tries_and_HuffmanCoding/00_Add_word_in_Tries.py
@@ -9,6 +9,7 @@ class Tries:
     def __init__(self) -> None:
         self.root = TriesNode()
 
+
     def insert(self, word):
         node = self.root
         for char in word:
@@ -17,6 +18,7 @@ class Tries:
             node = node.children[char]
         node.is_terminal = True
 
+
     def search(self, word):
         node = self.root
         for char in word:
@@ -24,7 +26,8 @@ class Tries:
                 return False
             node = node.children[char]
         return node.is_terminal
-    
+
+
     def starts_with(self, prefix):
         node = self.root
         for char in prefix:
@@ -34,18 +37,47 @@ class Tries:
         return True
     
 
+    def _delete_helper(self, node, word, index):
+        if index == len(word):
+            if node.is_terminal:
+                node.is_terminal = False
+                return True
+            return False
+        
+        char = word[index]
+        if char not in node.children:
+            return False
+        
+        can_delete = self._delete_helper(node.children[char], word, index+1)
+        if can_delete:
+            del node.children[char]
+            return not node.is_terminal and len(node.children) == 0
+        return False
+
+
+    def delete(self, word):
+        self._delete_helper(self.root, word, 0)
+
+
 def main():
     trie = Tries()
+
+    # adding word in tries
     trie.insert("apple")
     trie.insert("app")
     trie.insert("banana")
 
+    # searhinf word in tries
     print(trie.search("apple"))  
-    print(trie.search("app"))    
-    print(trie.search("banana")) 
-    print(trie.search("orange")) 
+    print(trie.search("appapp"))    
+
+    # word start with
     print(trie.starts_with("ban"))  
-    print(trie.starts_with("ora"))  
+    print(trie.starts_with("ora"))
+
+    # delete word in tries
+    trie.delete("apple")
+    print(trie.search("apple"))    
 
 
 if __name__ == '__main__':
